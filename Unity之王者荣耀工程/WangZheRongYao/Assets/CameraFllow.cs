7468ff34015b225e4b02916348a93b30bf860589
JayShang <yijieshang@gmail.com>
2018-04-27 17:40:08 +0800

    镜头跟随英雄移动而移动

---
+++ Unity之王者荣耀工程/WangZheRongYao/Assets/CameraFllow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFllow : MonoBehaviour {
+
+
+	[SerializeField]
+
+	private Transform player;
+
+	float cameraX;
+	float cameraZ;
+
+	public float y = 10;
+	public float z = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (player) {
+
+			cameraX = player.position.x;
+			cameraZ = player.position.z;
+			this.transform.position = new Vector3(cameraX,y,cameraZ + z);
+		
+		}
+	}
+}
