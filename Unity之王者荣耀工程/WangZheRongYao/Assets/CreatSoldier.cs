cc9c9ad79bd4b23595e538c24a2dd27d09ba0344
JayShang <yijieshang@gmail.com>
2018-04-28 17:09:51 +0800

    小兵生成

---
+++ Unity之王者荣耀工程/WangZheRongYao/Assets/CreatSoldier.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatSoldier : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject soldier;
+	[SerializeField]
+	private Transform starTran;
+
+	[SerializeField]
+	private Transform soldierParent;
+
+	bool isCreatSoldier = true;
+
+	public int soldierCount = 2;
+
+
+
+	void Start(){
+	
+		StartCoroutine (Creat(0,1,5));
+	}
+
+
+	void CreatSmartSoldier(){
+
+		GameObject obj =  Instantiate (soldier, starTran.position, Quaternion.identity)as GameObject ;
+		obj.transform.parent = soldierParent;
+
+		
+	}
+
+	private IEnumerator Creat (float time, float delyTime, float spwanTime){
+
+		yield return new WaitForSeconds (time);
+
+		while (isCreatSoldier) {
+		
+			for (int i = 0; i < soldierCount; i++) {
+			
+				CreatSmartSoldier ();
+				yield return new WaitForSeconds (delyTime);
+			
+			}
+
+			yield return new WaitForSeconds (spwanTime);
+
+
+
+
+		}
+
+
+	}
+
+
+}
